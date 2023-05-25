563cf188c591d46329d4214ebfee0603a8d5fc2a
ECWireless <elliott@coopallc.com>
2023-05-25 08:42:55 -0600

    Add basic Hasura route

--- events/guildMemberUpdate.js
+++ events/guildMemberUpdate.js
@@ -5,8 +5,6 @@ const { discordLogger } = require('../utils/logger');
 module.exports = {
   name: 'guildMemberUpdate',
   async execute(oldMember, newMember) {
-    console.log('guildMemberUpdate');
-
     try {
       if (
         !oldMember.roles.cache.has(SECRETS.MEMBER_ROLE_ID) &&
---
+++ routes/hasura.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const HASURA_ROUTER = express.Router();
+
+JOINUS_ROUTER.post('/create-channels', async (req, res) => {
+  console.log(req.body);
+});
+
+module.exports = HASURA_ROUTER;
