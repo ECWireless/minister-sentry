d561997eb3ae3f0d1c8655ba34f007ff9869500c
ECWireless <elliott@coopallc.com>
2023-06-08 18:58:33 -0600

    Log auth headers in payload

--- utils/auth.js
+++ utils/auth.js
@@ -7,6 +7,9 @@ const verifySignature = req => {
     .createHmac('sha256', SECRETS.WEBHOOK_API_TOKEN)
     .update(JSON.stringify(req.body))
     .digest('hex');
+
+  console.log('signature', `sha256=${signature}`);
+  console.log('x-hub-signature-256', req.headers.get('x-hub-signature-256'));
   return `sha256=${signature}` === req.headers.get('x-hub-signature-256');
 };
 
